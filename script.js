f61e3f1252797153d12dc2c46249b514c4d8c0a4
Abhi76311 <114270867+Abhi76311@users.noreply.github.com>
2024-10-14 22:17:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var h1 = document.querySelector('h1')
+
+h1.addEventListener("click", function () {
+    for (var a = 0; a < 100; a++) {
+        h1.innerHTML = "I LOVE U SOOO.... MUCH MERI BABU 😘😘❤️❤️🫠🫠😘😘😘😘😘😘😘😘😘😘❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️🫠🫠🫠🫠🫠🫠🫠🫠🫠🫠"
+        h1.style.backgroundColor = " crimson"
+        h1.style.fontSize="22px"
+    }
+})
