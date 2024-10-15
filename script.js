8dca333a4d9552fcbc59f6bb32673d78334b4731
Abhi76311 <114270867+Abhi76311@users.noreply.github.com>
2024-10-15 12:40:47 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var h1 = document.querySelector('h1')
 
 h1.addEventListener("click", function () {
     for (var a = 0; a < 100; a++) {
-        console.log ("I LOVE YOU" , a);
+        console.log("I love you",a);
         h1.innerHTML = "I LOVE U SOOO.... MUCH MERI BABU 😘😘❤️❤️🫠🫠😘😘😘😘😘😘😘😘😘😘❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️🫠🫠🫠🫠🫠🫠🫠🫠🫠🫠"
         h1.style.backgroundColor = " crimson"
         h1.style.fontSize="22px"
