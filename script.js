a25b87bacb42a3e6729663b1881c8c2328fa81f6
Abhi76311 <114270867+Abhi76311@users.noreply.github.com>
2024-10-15 12:34:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var h1 = document.querySelector('h1')
 
 h1.addEventListener("click", function () {
     for (var a = 0; a < 100; a++) {
+        console.log ("I LOVE YOU" , a);
         h1.innerHTML = "I LOVE U SOOO.... MUCH MERI BABU 😘😘❤️❤️🫠🫠😘😘😘😘😘😘😘😘😘😘❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️🫠🫠🫠🫠🫠🫠🫠🫠🫠🫠"
         h1.style.backgroundColor = " crimson"
         h1.style.fontSize="22px"
